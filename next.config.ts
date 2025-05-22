eef161e88699db1c2fe4f611c33d8b893f1fe54f
Abhishek Yadav <abhishekchd23@gmail.com>
2025-05-22 23:40:12 +0530

    config update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: '/amara',
+  assetPrefix: '/amara',
 };
 
 export default nextConfig;
